67c2a65a432f87d95c5112c941ea56cf064665d7
PriyanshuM2003 <studyx222@gmail.com>
2025-05-05 16:58:12 +0530

    minor changes

--- staylio-backend/backend/settings.py
+++ staylio-backend/backend/settings.py
@@ -46,10 +46,15 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8000", "http://127.0.0.1:3000"]
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
 
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOW_CREDENTIALS = True
 REST_AUTH = {"USE_JWT": True, "JWT_AUTH_HTTPONLY": False}
 
 # Application definition
